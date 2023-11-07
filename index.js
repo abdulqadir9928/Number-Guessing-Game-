575663fb86e02ea558f48aa8879d0fc805bcf0a8
Abdul Qadir <138937810+abdulqadir9928@users.noreply.github.com>
2023-11-07 07:16:12 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import chalk from "chalk";
+import inquirer from "inquirer";
+let genNumber = Math.floor(Math.random() * 2);
+let user = await inquirer.prompt({
+    name: 'number1',
+    type: 'list',
+    message: 'Choose a number',
+    choices: ["0", "1"]
+});
+if (genNumber == parseInt(user.number1)) {
+    console.log(chalk.bold.blueBright("Congratulations You Won!"));
+}
+else {
+    console.log(chalk.bold.redBright("You Lost!"));
+}
